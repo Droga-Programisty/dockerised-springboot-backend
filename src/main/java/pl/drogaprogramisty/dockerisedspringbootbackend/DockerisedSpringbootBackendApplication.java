dc1b4c06c4da51c73eb5de67de587a578dbfe900
lukaszdutka <lukaszdutka2@gmail.com>
2025-11-05 15:31:08 +0100

    Initial commit

---
+++ src/main/java/pl/drogaprogramisty/dockerisedspringbootbackend/DockerisedSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.drogaprogramisty.dockerisedspringbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerisedSpringbootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerisedSpringbootBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/drogaprogramisty/dockerisedspringbootbackend/DockerisedSpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.drogaprogramisty.dockerisedspringbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerisedSpringbootBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
